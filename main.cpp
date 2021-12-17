b8dedb56a71761f696b4e85cc70b34bf444b3fe4
dcruzwil <wilson.dcruz@hotmail.com>
2021-12-17 00:40:44 -0500

    .

--- main.cpp
+++ main.cpp
@@ -1,14 +1,78 @@
+/* We simply call the root header file "App.h", giving you uWS::App and uWS::SSLApp */
 #include "App.h"
+#include <time.h>
+#include <iostream>
+
+uWS::App* globalApp;
 
 int main() {
-	/* Overly simple hello world app */
-	uWS::App().get("/*", [](auto* res, auto*/*req*/) {
-			res->end("Hello world!");
-			}).listen(3000, [](auto* listen_socket) {
-				if (listen_socket) {
-					std::cout << "Listening on port " << 3000 << std::endl;
-				}
-				}).run();
+    /* ws->getUserData returns one of these */
+    struct PerSocketData {
+        std::string data;
+    };
 
-				std::cout << "Failed to listen on port 3000" << std::endl;
+    PerSocketData ps;
+    ps.data = "Hello";
+
+    /* Keep in mind that uWS::SSLApp({options}) is the same as uWS::App() when compiled without SSL support.
+     * You may swap to using uWS:App() if you don't need SSL */
+    uWS::App app = uWS::App().ws<PerSocketData>("/*", {
+            /* Settings */
+            .compression = uWS::SHARED_COMPRESSOR,
+            .maxPayloadLength = 16 * 1024 * 1024,
+            .idleTimeout = 16,
+            .maxBackpressure = 1 * 1024 * 1024,
+            .closeOnBackpressureLimit = false,
+            .resetIdleTimeoutOnSend = false,
+            .sendPingsAutomatically = true,
+            /* Handlers */
+            .upgrade = nullptr,
+            .open = [](auto* ws) {
+                auto d = ws->getUserData();
+                std::cout << d->data << "\n";
+                /* Open event here, you may access ws->getUserData() which points to a PerSocketData struct */
+                ws->subscribe("broadcast");
+            },
+            .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
+                std::cout << message << "\n";
+                auto d = ws->getUserData();
+                d->data = message;
+            },
+            .drain = [](auto*/*ws*/) {
+                /* Check ws->getBufferedAmount() here */
+            },
+            .ping = [](auto*/*ws*/, std::string_view) {
+                /* Not implemented yet */
+            },
+            .pong = [](auto*/*ws*/, std::string_view) {
+                /* Not implemented yet */
+            },
+            .close = [](auto*/*ws*/, int /*code*/, std::string_view /*message*/) {
+                /* You may access ws->getUserData() here */
+            }
+            }).listen(9001, [](auto* listen_socket) {
+                if (listen_socket) {
+                    std::cout << "Listening on port " << 9001 << std::endl;
+                }
+                });
+
+            struct us_loop_t* loop = (struct us_loop_t*)uWS::Loop::get();
+            struct us_timer_t* delayTimer = us_create_timer(loop, 0, 0);
+
+            // broadcast the unix time as millis every 8 millis
+            us_timer_set(delayTimer, [](struct us_timer_t*/*t*/) {
+
+                struct timespec ts;
+                timespec_get(&ts, TIME_UTC);
+
+                int64_t millis = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+
+                std::cout << "\rBroadcasting timestamp: " << millis; // << std::endl;
+
+                globalApp->publish("broadcast", std::string_view((char*)&millis, sizeof(millis)), uWS::OpCode::BINARY, false);
+                }, 5000, 1000);
+
+            globalApp = &app;
+
+            app.run();
 }
